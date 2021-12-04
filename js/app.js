367b2766632814ce1a5e377db1dca2be08604198
Nimesh Kumar <nimeshkumarteacher@gmail.com>
2021-12-04 23:06:36 +0530

    FILES UPLOADED

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("card-img-top");
+  //var modal_img = document.getElementsByClassName("modal-img");
+  var captionText = document.getElementById("caption");
+  var height=0,width=0;
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  //height=screen.height;
+  //modal_img[slideIndex-1].style.height= (height-300)+'px';
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
